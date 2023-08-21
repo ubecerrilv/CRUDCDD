9d11bac7fcdbca78f912b4510cd1ff234faf9dc9
Ulises Becerril <ulisesbecerril81@gmail.com>
2023-08-21 14:52:33 -0600

    Primer commit

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -2,9 +2,12 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,18 +16,21 @@ import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+import controlador.Comandos;
 
 
 @SuppressWarnings("serial")
 public class Ventana extends VentanaAGeneral{
-	
 	
 	JPanel panelCent;
 	JLabel paginas;
 	JTextArea pag;
 	JButton bBuscar;
 	JScrollPane sP;
+	GridBagConstraints rest;
 	
 	
 	public Ventana() {
@@ -36,8 +42,24 @@ public class Ventana extends VentanaAGeneral{
 			e.printStackTrace();
 		}
 		this.setExtendedState(MAXIMIZED_BOTH);
+		rest = new GridBagConstraints();
 		
 		//CREAR PANELES
+		
+		//EJEMPLO DE PANEL CON RESTRICCIONES
+		JPanel maxmin = new JPanel();
+		maxmin.setBorder (BorderFactory.createTitledBorder (BorderFactory.createEtchedBorder (),"Maximos y minimos del histograma",TitledBorder.CENTER,TitledBorder.TOP));
+		maxmin.setLayout(new GridBagLayout());
+		
+		JButton max = new JButton("Picos significativos del histograma");
+		max.setActionCommand(Comandos.INICIA);
+		max.addActionListener(this);
+		rest.gridx = 0;
+		rest.gridy = 0;
+		rest.gridwidth = 1;
+		rest.gridheight = 1;
+		maxmin.add(max, rest);
+		
 		//PANEL CENTRAL
 		panelCent = new JPanel(new GridLayout(1,2));
 		panelCent.setBorder(new EmptyBorder(5,5,5,5));
